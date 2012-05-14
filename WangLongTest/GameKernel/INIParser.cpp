9843533110f77ebefcd252222137cfbb97d2bd5e
Korman <Administrator@china-ef3f016fb.(none)>
2012-05-14 20:48:43 +0800

    Added ini file parser.

---
+++ WangLongTest/GameKernel/INIParser.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+#include "INIParser.h"
+
+BEGIN_KERNEL
+
+CINIParser::CINIParser(){}
+CINIParser::~CINIParser(){}
+
+END_KERNEL
---
+++ WangLongTest/GameKernel/Include/INIParser.h
@@ -0,0 +1,23 @@
+/*
+*
+*/
+
+#ifndef INIPARSER_H
+#define INIPARSER_H
+
+#include "GameKernelDefine.h"
+
+BEGIN_KERNEL
+
+class KERNEL_DLL CINIParser
+{
+public:
+	CINIParser();
+	virtual ~CINIParser();
+protected:
+private:
+};
+
+END_KERNEL
+
+#endif
