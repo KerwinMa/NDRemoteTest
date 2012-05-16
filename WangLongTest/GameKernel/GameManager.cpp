0749195a074689f23c519ec431626f01789bb0de
Korman <Administrator@china-ef3f016fb.(none)>
2012-05-16 23:33:44 +0800

    Coding...

--- WangLongTest/GameKernel/GameManager.cpp
+++ WangLongTest/GameKernel/GameManager.cpp
@@ -3,7 +3,45 @@
 
 BEGIN_KERNEL
 
-CGameManager::CGameManager(){}
+CGameManager* CGameManager::ms_pkGameManager = 0;
+CGameManager::CGameManager():m_pszGamePath(0),m_bIsInit(false){}
 CGameManager::~CGameManager(){}
+
+bool CGameManager::Create( const char* pszGameModulePath )
+{
+	CGameManager* pkGM = 0;
+	bool bSucceeded = false;
+
+	if (CGameManager::ms_pkGameManager)
+	{
+		return bSucceeded;
+	}
+
+	pkGM = new CGameManager;
+	bSucceeded = pkGM->Initialise(pszGameModulePath);
+
+	if (bSucceeded && pkGM)
+	{
+		CGameManager::ms_pkGameManager = pkGM;
+	}
+
+	return bSucceeded;
+}
+
+void CGameManager::Destroy()
+{
+	SafeDelete(CGameManager::ms_pkGameManager);
+}
+
+bool CGameManager::Initialise( const char* pszGameModulePath )
+{
+	if (0 == pszGameModulePath || !*pszGameModulePath)
+	{
+		return false;
+	}
+
+	m_bIsInit = true;
+	return true;
+}
 
 END_KERNEL
--- WangLongTest/GameKernel/Include/GameManager.h
+++ WangLongTest/GameKernel/Include/GameManager.h
@@ -12,9 +12,21 @@ BEGIN_KERNEL
 class KERNEL_DLL CGameManager
 {
 public:
+
 	CGameManager();
 	virtual ~CGameManager();
+
+	static bool Create(const char* pszGameModulePath);
+	static void Destroy();
+
 protected:
+
+	virtual bool Initialise(const char* pszGameModulePath);
+
+	INLINE_CONST_READONLY(char*,m_pszGamePath,GameModulePath);
+	INLINE_READONLY(bool,m_bIsInit,Initialised);
+	STATIC_READONLY(CGameManager*,ms_pkGameManager,GameManager);
+
 private:
 };
 
